5145e134a64082da04c8938be22b649ad6727a29
Slopez404 <santiago.lopeza@uniagustiniana.edu.co>
2023-03-06 15:50:49 -0500

    Initial commit

---
+++ app/src/main/java/com/example/tallerr3/ResultadoNota.java
@@ -0,0 +1,14 @@
+package com.example.tallerr3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResultadoNota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado_nota);
+    }
+}
